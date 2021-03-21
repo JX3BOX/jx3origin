66b8df6049041ff165578ccf03dfc0d0411f33bf
litthy <litthy@qq.com>
2021-03-21 23:11:09 +0800

    add index

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,32 +1,48 @@
 <template>
-    <nav class="m-nav">
-        <single_nav v-if="mode == 'single'" />
-        <list_nav />
-    </nav>
+  <nav class="m-nav">
+    <div class="m-nav-list">
+      <span class="m-nav-list-item" v-for="(item, i) in list" :key="i">
+        <router-link :to="item.link">{{ item.name }}</router-link>
+      </span>
+    </div>
+    <div class="m-nav-logo"></div>
+    <div class="m-nav-set"></div>
+  </nav>
 </template>
 
 <script>
-import single_nav from "./single_nav";
-import list_nav from "./list_nav";
+// import single_nav from "./single_nav";
+// import list_nav from "./list_nav";
 export default {
-    name: "Nav",
-    data: function() {
-        return {};
-    },
-    computed: {
-        mode: function() {
-            return this.$store.state.mode;
+  name: 'Nav',
+  data: function() {
+    return {
+      list: [
+        {
+          name: '新闻资讯',
+          link: './news',
         },
-    },
-    methods: {},
-    mounted: function() {},
-    components:{
-        single_nav,
-        list_nav,
+        {
+          name: '干货资料',
+          link: './news',
+        },
+        {
+          name: '休闲分享',
+          link: './news',
+        },
+      ],
     }
-};
+  },
+  computed: {},
+  methods: {},
+  mounted: function() {},
+  components: {
+    // single_nav,
+    // list_nav,
+  },
+}
 </script>
 
 <style lang="less">
-@import "../assets/css/nav.less";
+@import '../assets/css/nav.less';
 </style>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,27 +1,57 @@
 <template>
-    <div class="v-index">
+  <div class="v-index">
+    <div class="v-index-cont">
+      <div class="v-index-cont-search">
+        <el-input class="u-input-style" v-model="input" placeholder="输入想要搜索的内容"></el-input>
+ <el-button class="u-btn-style"></el-button>
+    
+      </div>
+      <div class="v-index-list">
+        <div class="v-index-list-qq">
+          <span class="u-txt">剑网3缘起官方交流Q群：672613096</span>
+          <span class="u-txt">魔盒官方交流Q群：3534604983</span>
+        </div>
+        <div class="v-index-list-box">
+          <div class="u-list-img"></div>
+          <div class="u-list-list">
+            <ul>
+              <li>
+                <a href="http://" target="_blank"><span class="u-title"> - 剑网3缘起于2020年1月3日正式公测！</span><span class="u-time">2020/01/03</span></a>
+              </li>
+              <li>
+                <a href="http://" target="_blank"><span class="u-title"> - 剑网3缘起于2020年1月3日正式公测！</span><span class="u-time">2020/01/03</span></a>
+              </li>
+              <li>
+                <a href="http://" target="_blank"><span class="u-title"> - 剑网3缘起于2020年1月3日正式公测！</span><span class="u-time">2020/01/03</span></a>
+              </li>
+              <li>
+                <a href="http://" target="_blank"><span class="u-title"> - 剑网3缘起于2020年1月3日正式公测！</span><span class="u-time">2020/01/03</span></a>
+              </li>
+            </ul>
+          </div>
+          <div class="u-list-box"></div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "Index",
-    props: [],
-    data: function() {
-        return {
-        };
-    },
-    computed: {
-    },
-    methods: {
-    },
-    created: function() {
-    },
-    components: {
-    },
-};
+  name: 'Index',
+  props: [],
+  data: function() {
+    return {
+      input: '',
+    }
+  },
+  computed: {},
+  methods: {},
+  created: function() {},
+  components: {},
+}
 </script>
 
 <style lang="less">
-@import "../assets/css/index.less";
+@import '../assets/css/index.less';
 </style>
